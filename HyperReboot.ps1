a9a8ca632b8f4c87ba3fe48c2b8658fbb93485cc
ゆき <33691365+YukiLeafX@users.noreply.github.com>
2020-12-25 21:53:15 +0900

    スクリプトの追加

---
+++ HyperReboot.ps1
@@ -0,0 +1,29 @@
+Param(
+    [Switch]$DownAllVM = $false,
+    [Switch]$RebootSBRouter = $false,
+    [Switch]$OSReboot = $false,
+    [Switch]$WaitSBRouter = $false,
+    [Switch]$UpAllVM = $false
+)
+
+if ($DownAllVM) {
+    Get-VM | ? {$_.State -eq "Running"} | Stop-VM
+}
+
+if ($RebootSBRouter) {
+    Invoke-WebRequest -UseBasicParsing "http://192.168.3.1/stay.html"
+}
+
+if ($OSReboot) {
+    shutdown /r /t 0 /f /d p:4:1
+}
+
+if ($WaitSBRouter) {
+    do {
+        Write-Host "Waiting..."
+    } until (Test-Connection "192.168.3.1" -Count 1 -Quiet)
+}
+
+if ($UpAllVM) {
+    Get-VM | ? {$_.State -eq "Off"} | Start-VM
+}
